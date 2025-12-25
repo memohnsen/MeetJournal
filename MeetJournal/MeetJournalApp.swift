348c4ae8e584ea92e257a0d50bce53193d393260
Maddisen Mohnsen <memohnsen@gmail.com>
2025-12-25 09:49:41 -0500

    Initial Commit

---
+++ MeetJournal/MeetJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetJournalApp.swift
+//  MeetJournal
+//
+//  Created by Maddisen Mohnsen on 12/25/25.
+//
+
+import SwiftUI
+
+@main
+struct MeetJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
